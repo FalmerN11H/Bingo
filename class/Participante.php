e832ebf9083fc36687efea87b5780803c6377565
FalmerN11H <ro-5000@hotmail.com>
2019-05-23 20:12:58 -0300

    14.0

--- class/Participante.php
+++ class/Participante.php
@@ -1,5 +1,11 @@
 <?php
 
+//Bianca Pereira da Silva - 20989373
+//Bruno Milano Pedroso da Silva - 21009643
+//Falmer Rodrigo Venancio Nieto - 20955356
+//Thiago Rezende Santos - 20919872
+//Wurdolf Andrews Silva Dias - 20974511
+
 class Participante{
 	private $ra;
 	private $nome;
